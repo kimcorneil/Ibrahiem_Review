fd50a662bde068d471007879a96fb8832401f582
ibrahimemam7 <1ibrahimemam@gmail.com>
2024-06-07 18:09:52 -0400

    Initial commit

---
+++ push_test.R
@@ -0,0 +1,8 @@
+#'this is a test script
+#'the '#'' notation helps with multi-line comments
+
+myvector <- seq(100, 1, by=-7)
+
+myavg <- mean(myvector)
+mysum <- sum(myvector)
+mysd <- sd(myvector)
