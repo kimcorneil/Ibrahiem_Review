54eb6fa8ee3dd1de26c32d94b59e3c43e016e2cd
ibrahimemam7 <1ibrahimemam@gmail.com>
2024-06-17 22:40:15 -0400

    Improved comments and style

--- assignment_2/assignment_2.R
+++ assignment_2/assignment_2.R
@@ -6,29 +6,40 @@
 number_string <- readline(prompt = "Enter a three digit positive number: ")
 
 #make a numeric object containing the user's input
-number_numeric <- as.numeric(number_string) #Note: maybe supress warning since we check for NAs anyway?
+#"NAs introduced by coercion" warning is suppressed because this possibility is handled later on
+number_numeric <- suppressWarnings(as.numeric(number_string))
 
-#ensure the user's input is numeric, otherwise quit the R script
-if(is.na(number_string)) {
-  stop("input must be numeric.")
-} else if(number_numeric > 999 | number_numeric %% 1 != 0 | number_numeric < 0) {
-  stop("input must be a positive whole number with 3 digits.")
+#check if the number is NA, as this would be the result of a non-numeric input
+if(!is.na(number_numeric)) {
+  
+  #if the input was numeric (i.e. not NA), ensure the number is 3 digits, positive, and whole.
+  if(number_numeric > 100 & number_numeric < 1000 & number_numeric %% 1 == 0) {
+    
+    #split the number into its individual digits, then change from list to vector format
+    digits <- unlist(strsplit(number_string, ""))
+  
+    #convert the digits vector from character to numeric
+    digits <- as.numeric(digits)
+    
+    #cube each digit in the vector then sum the results
+    cube_sum <- sum(digits^3)
+    
+    #check if the number entered by the user is equal to the sum of the digit's cubes
+    if(cube_sum == number_numeric) {
+      
+      cat(number_numeric, "is an Armstrong number.")
+      
+    } else {
+      
+      cat(number_numeric, "is NOT an Armstrong number.")
+    }
+  } else {
+    
+    #the input of the user was numeric but not a whole, positive number with 3 digits
+    print("Input must be a positive whole number with 3 digits.")
+  }
 } else {
-  #NOTE learned from class: should start with the math below under the first if statement
-}
-
-#split the number into its individual digits, then change from list to vector format
-digits <- unlist(strsplit(number_string, ""))
-
-#convert the digits vector from character to numeric
-digits <- as.numeric(digits)
-
-#cube each digit in the vector then sum the results
-cube_sum <- sum(digits^3)
-
-#check if the number entered by the user is equal to the sum of the digit's cubes
-if(cube_sum == number_numeric) {
-  cat(number_numeric, "is an Armstrong number.")
-} else {
-  cat(number_numeric, "is NOT an Armstrong number.")
+  
+  #the input was not numeric and therefore got converted to NA
+  print("Input must be numeric.")
 }
